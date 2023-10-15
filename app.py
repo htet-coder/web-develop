2a5078a4ba766fb44bd2ad607ccd3255e44baca1
htet-coder <71670671+htet-coder@users.noreply.github.com>
2023-10-15 19:18:19 +0000

    Added Dynamic Data and api route

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title':'Data Analyst',
+        'salary':'ks. 1200000',
+        'country':'Myanmar'
+    },
+    {
+        'id': 2,
+        'title':'GIS Officer',
+        'country':'Thailand'
+    },
+    {
+        'id': 3,
+        'title':'Remote Sensing Specialist',
+        'salary':'$ 1500000',
+        'country':'Remote'
+    },
+        {
+        'id': 4,
+        'title':'Trainer',
+        'salary':'$ 1300000',
+        'country':'Califonia'
+    }
+]
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, company='Futrue Perspective Myanmar')
+
+@app.route('/api/jobs')
+def job_list():
+    return jsonify(JOBS)
 
 if __name__ == '__main__':
     app.run(debug=True)
