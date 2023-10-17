3ab52663dab27b51432f17a01550a26ccd718bd8
htet-coder <71670671+htet-coder@users.noreply.github.com>
2023-10-17 10:05:36 +0000

    Modified the home page.

--- app.py
+++ app.py
@@ -1,36 +1,49 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, url_for, redirect
 
 app = Flask(__name__)
 
 JOBS = [
     {
         'id': 1,
-        'title':'Data Analyst',
-        'salary':'ks. 1200000',
-        'country':'Myanmar'
+        'title':'QGIS Basic Training',
+        'fee':'ks. 40,000',
+        'duration': '3 month',
+        'type':'In-Person'
     },
     {
         'id': 2,
-        'title':'GIS Officer',
-        'country':'Thailand'
+        'title':'QGIS Intermediate Training',
+        'fee':'ks. 30,000',
+        'duration': '2 month',
+        'type':'Online'
     },
     {
         'id': 3,
-        'title':'Remote Sensing Specialist',
-        'salary':'$ 1500000',
-        'country':'Remote'
+        'title':'Basic Python Programming Training',
+        'fee':'ks. 40,000',
+        'duration': '3 month',
+        'type':'In-Person'
     },
         {
         'id': 4,
-        'title':'Trainer',
-        'salary':'$ 1300000',
-        'country':'Califonia'
+        'title':'Data Management Traning',
+        'fee':'ks. 40,000',
+        'duration': '3 month',
+        'type':'In-Person'
     }
 ]
 
 @app.route('/')
 def home():
     return render_template('home.html', jobs=JOBS, company='Futrue Perspective Myanmar')
+
+@app.route('/home', methods=['GET', 'POST'])
+def home_page():
+    return render_template('home.html', jobs=JOBS, company='Futrue Perspective Myanmar')
+
+@app.route('/service', methods=['GET', 'POST'])
+def services():
+    return render_template('services.html')
 
 @app.route('/api/jobs')
 def job_list():
